f783135db59e2a9c95115f97d27c13decf1f2fa9
PonyJackal <pony1125@outlook.com>
2022-10-06 17:10:51 -0700

    feat: add goerli

--- contracts/StakeNFT.sol
+++ contracts/StakeNFT.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.4;
 
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract StakeNFT is ERC721A, Ownable {
     string private _baseTokenURI;
@@ -21,6 +22,16 @@ contract StakeNFT is ERC721A, Ownable {
 
     function setBaseURI(string calldata baseURI) external onlyOwner {
         _baseTokenURI = baseURI;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length != 0 ? string(abi.encodePacked(baseURI, Strings.toString(tokenId), ".json")) : "";
     }
 
     /** MUTATIVE FUNCTIONS */
